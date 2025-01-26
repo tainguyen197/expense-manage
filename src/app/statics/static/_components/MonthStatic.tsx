d02ee174cb4f592a17edffdb27a40859ae438cd6
Nguyen Trung Tai <tainguyen197.ntt@gmail.com>
2025-01-26 11:06:45 +0700

    feat: put month year to calendar

--- src/app/statics/static/_components/MonthStatic.tsx
+++ src/app/statics/static/_components/MonthStatic.tsx
@@ -1,10 +1,6 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { CalendarDays, ChevronDown } from "lucide-react";
 import { Progress } from "@/components/ui/progress";
 import {
@@ -12,7 +8,6 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { Button } from "@/components/ui/button";
 import {
   Select,
   SelectContent,
@@ -22,8 +17,91 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useRouter, useSearchParams } from "next/navigation";
+import { routesConfig } from "@/config/routes";
+import React from "react";
+
+const monthName = [
+  {
+    name: "January",
+    value: "01",
+  },
+  {
+    name: "February",
+    value: "02",
+  },
+  {
+    name: "March",
+    value: "03",
+  },
+  {
+    name: "April",
+    value: "04",
+  },
+  {
+    name: "May",
+    value: "05",
+  },
+  {
+    name: "June",
+    value: "06",
+  },
+  {
+    name: "July",
+    value: "07",
+  },
+  {
+    name: "August",
+    value: "08",
+  },
+  {
+    name: "September",
+    value: "09",
+  },
+  {
+    name: "October",
+    value: "10",
+  },
+  {
+    name: "November",
+    value: "11",
+  },
+  {
+    name: "December",
+    value: "12",
+  },
+];
+
+const yearName = ["2024", "2025", "2026", "2027", "2028"];
 
 const MonthStatic = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleValueChange = (params: Record<string, string>) => {
+    const currentParams = new URLSearchParams(searchParams.toString());
+    Object.entries(params).forEach(([key, value]) => {
+      currentParams.set(key, value);
+    });
+    router.push(`${routesConfig.statics.static}?${currentParams.toString()}`);
+  };
+
+  React.useEffect(() => {
+    // set default month and year by current date if not exist
+    if (!searchParams.get("month") || !searchParams.get("year")) {
+      const date = new Date();
+      const month = (date.getMonth() + 1).toString().padStart(2, "0");
+      const year = date.getFullYear().toString();
+      handleValueChange({ year, month });
+    }
+  }, [searchParams]);
+
+  const currentMonth = searchParams.get("month");
+  const currentMonthName = monthName.find(
+    (month) => month.value === currentMonth
+  );
+  const currentYear = searchParams.get("year");
+
   return (
     <Card>
       <CardHeader>
@@ -31,38 +109,46 @@ const MonthStatic = () => {
           <PopoverTrigger asChild>
             <CardTitle className="flex items-end text-md gap-2">
               <CalendarDays />{" "}
-              <span className="text-lg font-bold leading-4">July 2024</span>
+              <span className="text-lg font-bold leading-4">
+                {currentMonthName?.name} {currentYear}
+              </span>
               <ChevronDown size={16} />
             </CardTitle>
           </PopoverTrigger>
           <PopoverContent className="w-80 flex gap-4">
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Select a fruit" />
+            <Select
+              defaultValue={currentMonthName?.value}
+              onValueChange={(value) => handleValueChange({ month: value })}
+            >
+              <SelectTrigger className="border-none">
+                <SelectValue placeholder="Select a month" />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
-                  <SelectLabel>Fruits</SelectLabel>
-                  <SelectItem value="apple">Apple</SelectItem>
-                  <SelectItem value="banana">Banana</SelectItem>
-                  <SelectItem value="blueberry">Blueberry</SelectItem>
-                  <SelectItem value="grapes">Grapes</SelectItem>
-                  <SelectItem value="pineapple">Pineapple</SelectItem>
+                  <SelectLabel>Month</SelectLabel>
+                  {monthName.map((month) => (
+                    <SelectItem key={month.value} value={month.value}>
+                      {month.name}
+                    </SelectItem>
+                  ))}
                 </SelectGroup>
               </SelectContent>
             </Select>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Select a fruit" />
+            <Select
+              defaultValue={currentYear ?? undefined}
+              onValueChange={(value) => handleValueChange({ year: value })}
+            >
+              <SelectTrigger className="border-none">
+                <SelectValue placeholder="Select a year" />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
-                  <SelectLabel>Fruits</SelectLabel>
-                  <SelectItem value="apple">Apple</SelectItem>
-                  <SelectItem value="banana">Banana</SelectItem>
-                  <SelectItem value="blueberry">Blueberry</SelectItem>
-                  <SelectItem value="grapes">Grapes</SelectItem>
-                  <SelectItem value="pineapple">Pineapple</SelectItem>
+                  <SelectLabel>Year</SelectLabel>
+                  {yearName.map((year) => (
+                    <SelectItem key={year} value={year}>
+                      {year}
+                    </SelectItem>
+                  ))}
                 </SelectGroup>
               </SelectContent>
             </Select>
---
+++ src/config/routes.ts
@@ -0,0 +1,10 @@
+export const routesConfig = {
+  home: "/",
+  chat: "/chat",
+  expenses: "/expenses",
+  notFound: "/404",
+  statics: {
+    __pathname: "/statics",
+    static: "/statics/static",
+  },
+};
